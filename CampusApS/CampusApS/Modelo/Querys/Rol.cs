313f21935ea31a7983423d86e7c8cc15fcb7e4f4
Alejandro <alexrametespinosa@gmail.com>
2020-12-07 17:40:40 +0100

    querys de nuevo

---
+++ CampusApS/CampusApS/Modelo/Querys/Rol.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Rol
+    {
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { }
+        }
+    }
+}
