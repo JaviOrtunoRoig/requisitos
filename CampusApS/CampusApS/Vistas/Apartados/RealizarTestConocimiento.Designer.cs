466f2b530efd0a52ab719733d7cbd68c82231d0e
Javier <javi_roig@hotmail.es>
2021-01-10 19:08:44 +0100

    Mas vistas

---
+++ CampusApS/CampusApS/Vistas/Apartados/RealizarTestConocimiento.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace CampusApS.Vistas.Apartados {
+    partial class RealizarTestConocimiento {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RealizarTestConocimiento));
+            this.tNombreTest = new System.Windows.Forms.Label();
+            this.tPregunta1 = new System.Windows.Forms.Label();
+            this.tPregunta2 = new System.Windows.Forms.Label();
+            this.tPregunta3 = new System.Windows.Forms.Label();
+            this.tPregunta4 = new System.Windows.Forms.Label();
+            this.tPregunta5 = new System.Windows.Forms.Label();
+            this.clPregunta1 = new System.Windows.Forms.CheckedListBox();
+            this.clPregunta2 = new System.Windows.Forms.CheckedListBox();
+            this.clPregunta3 = new System.Windows.Forms.CheckedListBox();
+            this.clPregunta4 = new System.Windows.Forms.CheckedListBox();
+            this.clPregunta5 = new System.Windows.Forms.CheckedListBox();
+            this.xuiButton1 = new XanderUI.XUIButton();
+            this.SuspendLayout();
+            // 
+            // tNombreTest
+            // 
+            this.tNombreTest.AutoSize = true;
+            this.tNombreTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tNombreTest.ForeColor = System.Drawing.Color.White;
+            this.tNombreTest.Location = new System.Drawing.Point(62, 47);
+            this.tNombreTest.Name = "tNombreTest";
+            this.tNombreTest.Size = new System.Drawing.Size(45, 18);
+            this.tNombreTest.TabIndex = 0;
+            this.tNombreTest.Text = "titulo";
+            // 
+            // tPregunta1
+            // 
+            this.tPregunta1.AutoSize = true;
+            this.tPregunta1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tPregunta1.ForeColor = System.Drawing.Color.White;
+            this.tPregunta1.Location = new System.Drawing.Point(62, 90);
+            this.tPregunta1.Name = "tPregunta1";
+            this.tPregunta1.Size = new System.Drawing.Size(41, 15);
+            this.tPregunta1.TabIndex = 1;
+            this.tPregunta1.Text = "label1";
+            // 
+            // tPregunta2
+            // 
+            this.tPregunta2.AutoSize = true;
+            this.tPregunta2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tPregunta2.ForeColor = System.Drawing.Color.White;
+            this.tPregunta2.Location = new System.Drawing.Point(62, 213);
+            this.tPregunta2.Name = "tPregunta2";
+            this.tPregunta2.Size = new System.Drawing.Size(41, 15);
+            this.tPregunta2.TabIndex = 2;
+            this.tPregunta2.Text = "label1";
+            // 
+            // tPregunta3
+            // 
+            this.tPregunta3.AutoSize = true;
+            this.tPregunta3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tPregunta3.ForeColor = System.Drawing.Color.White;
+            this.tPregunta3.Location = new System.Drawing.Point(62, 327);
+            this.tPregunta3.Name = "tPregunta3";
+            this.tPregunta3.Size = new System.Drawing.Size(41, 15);
+            this.tPregunta3.TabIndex = 3;
+            this.tPregunta3.Text = "label1";
+            // 
+            // tPregunta4
+            // 
+            this.tPregunta4.AutoSize = true;
+            this.tPregunta4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tPregunta4.ForeColor = System.Drawing.Color.White;
+            this.tPregunta4.Location = new System.Drawing.Point(60, 453);
+            this.tPregunta4.Name = "tPregunta4";
+            this.tPregunta4.Size = new System.Drawing.Size(41, 15);
+            this.tPregunta4.TabIndex = 4;
+            this.tPregunta4.Text = "label1";
+            // 
+            // tPregunta5
+            // 
+            this.tPregunta5.AutoSize = true;
+            this.tPregunta5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tPregunta5.ForeColor = System.Drawing.Color.White;
+            this.tPregunta5.Location = new System.Drawing.Point(60, 584);
+            this.tPregunta5.Name = "tPregunta5";
+            this.tPregunta5.Size = new System.Drawing.Size(41, 15);
+            this.tPregunta5.TabIndex = 5;
+            this.tPregunta5.Text = "label1";
+            // 
+            // clPregunta1
+            // 
+            this.clPregunta1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(173)))), ((int)(((byte)(242)))));
+            this.clPregunta1.ForeColor = System.Drawing.Color.White;
+            this.clPregunta1.FormattingEnabled = true;
+            this.clPregunta1.Location = new System.Drawing.Point(65, 120);
+            this.clPregunta1.Name = "clPregunta1";
+            this.clPregunta1.Size = new System.Drawing.Size(476, 79);
+            this.clPregunta1.TabIndex = 12;
+            // 
+            // clPregunta2
+            // 
+            this.clPregunta2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(173)))), ((int)(((byte)(242)))));
+            this.clPregunta2.ForeColor = System.Drawing.Color.White;
+            this.clPregunta2.FormattingEnabled = true;
+            this.clPregunta2.Location = new System.Drawing.Point(63, 244);
+            this.clPregunta2.Name = "clPregunta2";
+            this.clPregunta2.Size = new System.Drawing.Size(476, 79);
+            this.clPregunta2.TabIndex = 13;
+            // 
+            // clPregunta3
+            // 
+            this.clPregunta3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(173)))), ((int)(((byte)(242)))));
+            this.clPregunta3.ForeColor = System.Drawing.Color.White;
+            this.clPregunta3.FormattingEnabled = true;
+            this.clPregunta3.Location = new System.Drawing.Point(63, 357);
+            this.clPregunta3.Name = "clPregunta3";
+            this.clPregunta3.Size = new System.Drawing.Size(476, 79);
+            this.clPregunta3.TabIndex = 14;
+            // 
+            // clPregunta4
+            // 
+            this.clPregunta4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(173)))), ((int)(((byte)(242)))));
+            this.clPregunta4.ForeColor = System.Drawing.Color.White;
+            this.clPregunta4.FormattingEnabled = true;
+            this.clPregunta4.Location = new System.Drawing.Point(63, 485);
+            this.clPregunta4.Name = "clPregunta4";
+            this.clPregunta4.Size = new System.Drawing.Size(476, 79);
+            this.clPregunta4.TabIndex = 15;
+            // 
+            // clPregunta5
+            // 
+            this.clPregunta5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(173)))), ((int)(((byte)(242)))));
+            this.clPregunta5.ForeColor = System.Drawing.Color.White;
+            this.clPregunta5.FormattingEnabled = true;
+            this.clPregunta5.Location = new System.Drawing.Point(63, 615);
+            this.clPregunta5.Name = "clPregunta5";
+            this.clPregunta5.Size = new System.Drawing.Size(476, 79);
+            this.clPregunta5.TabIndex = 16;
+            // 
+            // xuiButton1
+            // 
+            this.xuiButton1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(70)))), ((int)(((byte)(75)))));
+            this.xuiButton1.ButtonImage = ((System.Drawing.Image)(resources.GetObject("xuiButton1.ButtonImage")));
+            this.xuiButton1.ButtonStyle = XanderUI.XUIButton.Style.Dark;
+            this.xuiButton1.ButtonText = "Enviar";
+            this.xuiButton1.ClickBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(75)))), ((int)(((byte)(80)))));
+            this.xuiButton1.ClickTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(130)))), ((int)(((byte)(140)))));
+            this.xuiButton1.CornerRadius = 5;
+            this.xuiButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.xuiButton1.Horizontal_Alignment = System.Drawing.StringAlignment.Center;
+            this.xuiButton1.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(80)))), ((int)(((byte)(90)))));
+            this.xuiButton1.HoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(215)))));
+            this.xuiButton1.ImagePosition = XanderUI.XUIButton.imgPosition.Left;
+            this.xuiButton1.Location = new System.Drawing.Point(184, 718);
+            this.xuiButton1.Name = "xuiButton1";
+            this.xuiButton1.Size = new System.Drawing.Size(200, 50);
+            this.xuiButton1.TabIndex = 31;
+            this.xuiButton1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(200)))), ((int)(((byte)(185)))));
+            this.xuiButton1.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            // 
+            // RealizarTestConocimiento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(173)))), ((int)(((byte)(242)))));
+            this.ClientSize = new System.Drawing.Size(603, 780);
+            this.Controls.Add(this.xuiButton1);
+            this.Controls.Add(this.clPregunta5);
+            this.Controls.Add(this.clPregunta4);
+            this.Controls.Add(this.clPregunta3);
+            this.Controls.Add(this.clPregunta2);
+            this.Controls.Add(this.clPregunta1);
+            this.Controls.Add(this.tPregunta5);
+            this.Controls.Add(this.tPregunta4);
+            this.Controls.Add(this.tPregunta3);
+            this.Controls.Add(this.tPregunta2);
+            this.Controls.Add(this.tPregunta1);
+            this.Controls.Add(this.tNombreTest);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "RealizarTestConocimiento";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RealizarTestConocimiento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tNombreTest;
+        private System.Windows.Forms.Label tPregunta1;
+        private System.Windows.Forms.Label tPregunta2;
+        private System.Windows.Forms.Label tPregunta3;
+        private System.Windows.Forms.Label tPregunta4;
+        private System.Windows.Forms.Label tPregunta5;
+        private System.Windows.Forms.CheckedListBox clPregunta1;
+        private System.Windows.Forms.CheckedListBox clPregunta2;
+        private System.Windows.Forms.CheckedListBox clPregunta3;
+        private System.Windows.Forms.CheckedListBox clPregunta4;
+        private System.Windows.Forms.CheckedListBox clPregunta5;
+        private XanderUI.XUIButton xuiButton1;
+    }
+}
